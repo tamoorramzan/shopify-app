511701da6b118034aeb224ae2f410351f5b91ed6
tamoor-bemeir <tamoor@bemeir.com>
2023-01-18 10:37:29 +0500

    Validation

--- resources/views/Products/add.blade.php
+++ resources/views/Products/add.blade.php
@@ -3,6 +3,16 @@
 <div class="container">
 <h1>Add product data</h1>
 <form action="{{ route('product.store') }}" method="POST">
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <strong>Whoops!</strong> There were some problems with your input.<br><br>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
     @csrf
      <div class="row">
         <div class="col-xs-12 col-sm-12 col-md-12">
